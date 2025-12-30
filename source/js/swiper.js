b5b68892ad32d14f4d85cf0f013f224e5b35f3ed
Elisaveta Ustiuzhanina <yaarbuz@gmail.com>
2025-12-30 21:19:05 +0300

    Добавляет адаптив

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,10 +1,8 @@
 import Swiper from 'swiper';
-import { Pagination, Scrollbar, Navigation } from 'swiper/modules';
-
-import 'swiper/css';
+import { Pagination, Navigation, Scrollbar, Grid } from 'swiper/modules';
 
 export const initHeroSwiper = () => new Swiper('.hero__swiper', {
-  modules: [Pagination, Scrollbar],
+  modules: [Pagination],
   loop: true,
   speed: 600,
 
@@ -21,32 +19,72 @@ export const initHeroSwiper = () => new Swiper('.hero__swiper', {
 });
 
 export const initProgramsSwiper = () => new Swiper('.programs__swiper', {
-  modules: [Navigation],
+  modules: [Navigation, Scrollbar],
   loop: false,
   speed: 600,
 
   slidesPerView: 1,
-  spaceBetween: 15,
+  spaceBetween: 30,
 
   navigation: {
     nextEl: '.programs .swiper-button-next',
     prevEl: '.programs .swiper-button-prev',
     disabledClass: 'swiper-button--deactivated',
-  }
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    hide: false,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+    },
+    1440: {
+      allowTouchMove: false,
+      slidesPerView: 'auto',
+      spaceBetween: 32,
+    },
+  },
 });
 
 export const initNewsSwiper = () => new Swiper('.news__swiper', {
-  modules: [Navigation],
+  modules: [Navigation, Pagination, Grid],
   loop: false,
   speed: 600,
 
+  pagination: {
+    el: '.news__swiper .swiper-pagination',
+    clickable: true,
+  },
+
   slidesPerView: 1,
   spaceBetween: 15,
+
+  grid: {
+    rows: 2,
+  },
 
   navigation: {
     nextEl: '.news .swiper-button-next',
     prevEl: '.news .swiper-button-prev',
     disabledClass: 'swiper-button--deactivated',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+      grid: {
+        rows: 1,
+      },
+    },
   },
 });
 
@@ -62,5 +100,11 @@ export const initReviewsSwiper = () => new Swiper('.reviews__swiper', {
     nextEl: '.reviews .swiper-button-next',
     prevEl: '.reviews .swiper-button-prev',
     disabledClass: 'swiper-button--deactivated',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
   },
 });
